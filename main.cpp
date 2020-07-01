62aa32bbc22dff007d3390182e8f87c73d284353
dvdemon <dvdemon@hotmail.com>
2020-07-01 12:42:57 +0300

    project structure

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "web_server/httpwebserver.h"
+
+auto main(int argc,char* argv[]) -> int{
+    HTTPWebServer app;
+    return app.run(argc, argv);
+
+}
---
+++ web_server/httpwebserver.h
@@ -0,0 +1,121 @@
+#ifndef HTTPWEBSERVER_H
+#define HTTPWEBSERVER_H
+
+#include "Poco/Net/HTTPServer.h"
+#include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Net/HTTPRequestHandlerFactory.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include "Poco/Net/HTTPServerResponse.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/ServerSocket.h"
+#include "Poco/Timestamp.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/DateTimeFormat.h"
+#include "Poco/Exception.h"
+#include "Poco/ThreadPool.h"
+#include "Poco/Util/ServerApplication.h"
+#include "Poco/Util/Option.h"
+#include "Poco/Util/OptionSet.h"
+#include "Poco/Util/HelpFormatter.h"
+#include <iostream>
+
+#define UNUSED(value) (void)value
+
+using Poco::Net::ServerSocket;
+using Poco::Net::HTTPRequestHandler;
+using Poco::Net::HTTPRequestHandlerFactory;
+using Poco::Net::HTTPServer;
+using Poco::Net::HTTPServerRequest;
+using Poco::Net::HTTPServerResponse;
+using Poco::Net::HTTPServerParams;
+using Poco::Timestamp;
+using Poco::DateTimeFormatter;
+using Poco::DateTimeFormat;
+using Poco::ThreadPool;
+using Poco::Util::ServerApplication;
+using Poco::Util::Application;
+using Poco::Util::Option;
+using Poco::Util::OptionSet;
+using Poco::Util::OptionCallback;
+using Poco::Util::HelpFormatter;
+
+#include "httprequestfactory.h"
+
+
+class HTTPWebServer: public Poco::Util::ServerApplication
+{
+public:
+    HTTPWebServer(): _helpRequested(false)
+    {
+    }
+
+    ~HTTPWebServer()
+    {
+    }
+
+protected:
+    void initialize(Application& self)
+    {
+        loadConfiguration();
+        ServerApplication::initialize(self);
+    }
+
+    void uninitialize()
+    {
+        ServerApplication::uninitialize();
+    }
+
+    void defineOptions(OptionSet& options)
+    {
+        ServerApplication::defineOptions(options);
+
+        options.addOption(
+        Option("help", "h", "display argument help information")
+            .required(false)
+            .repeatable(false)
+            .callback(OptionCallback<HTTPWebServer>(
+                this, &HTTPWebServer::handleHelp)));
+    }
+
+    void handleHelp(const std::string& name, 
+                    const std::string& value)
+    {
+        HelpFormatter helpFormatter(options());
+        helpFormatter.setCommand(commandName());
+        helpFormatter.setUsage("OPTIONS");
+        helpFormatter.setHeader(
+            "A web server that serves the current date and time.");
+        helpFormatter.format(std::cout);
+        stopOptionsProcessing();
+        _helpRequested = true;
+
+        UNUSED(name);
+        UNUSED(value);
+    }
+
+    int main(const std::vector<std::string>& args)
+    {
+        if (!_helpRequested)
+        {
+            unsigned short port = (unsigned short)
+                config().getInt("HTTPWebServer.port", 9980);
+            std::string format(
+                config().getString("HTTPWebServer.format", 
+                                   DateTimeFormat::SORTABLE_FORMAT));
+
+            ServerSocket svs(port);
+            HTTPServer srv(new HTTPRequestFactory(format), 
+                                svs, new HTTPServerParams);
+            srv.start();
+            waitForTerminationRequest();
+            srv.stop();
+        }
+        UNUSED(args);
+        return Application::EXIT_OK;
+    }
+
+private:
+    bool _helpRequested;
+};
+#endif // !HTTPWEBSERVER
