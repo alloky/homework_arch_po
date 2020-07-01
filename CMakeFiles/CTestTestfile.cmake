e9cebf1b93f5397dcf9c17f53d7711287a661faa
dvdemon <dvdemon@hotmail.com>
2020-07-01 12:47:02 +0300

    trash

--- CMakeFiles/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dvdemon/OneDrive/Documents/Lectures/OOP/2020/Highload/hl_homework_01
-# Build directory: /Users/dvdemon/OneDrive/Documents/Lectures/OOP/2020/Highload/hl_homework_01/CMakeFiles
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtests "gtests")
-set_tests_properties(gtests PROPERTIES  _BACKTRACE_TRIPLES "/Users/dvdemon/OneDrive/Documents/Lectures/OOP/2020/Highload/hl_homework_01/CMakeLists.txt;51;add_test;/Users/dvdemon/OneDrive/Documents/Lectures/OOP/2020/Highload/hl_homework_01/CMakeLists.txt;0;")
