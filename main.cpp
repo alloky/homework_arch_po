35eb87b35ddb5435a41eaa5e10270eec3010ba48
dvdemon <dvdemon@hotmail.com>
2020-07-09 16:30:04 +0300

    azure

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 
 #include "web_server/httpwebserver.h"
 
+#include "database/friends.h"
+#include "database/person.h"
+
 auto main(int argc, char *argv[]) -> int{
+
+    std::cout << "MySQL version:" << database::Database_MySQL::get_version() << std::endl;
+    database::Database_MySQL::get();
+    database::Person::init();
+    database::Friends::init();
+
     HTTPWebServer app;
     return app.run(argc, argv);
 
