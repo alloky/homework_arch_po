7884c65b99a25e196633bdce98db9f1fd532c440
dvdemon <dvdemon@hotmail.com>
2020-07-15 15:23:54 +0300

    database connection

--- database/database_mysql.cpp
+++ database/database_mysql.cpp
@@ -3,41 +3,73 @@
 #include <iostream>
 
 database::Database_MySQL::Database_MySQL(){
-    std::cout << "connecting to mysql ... " << std::endl;
+    connect();
+}
+
+void database::Database_MySQL::connect(){
+    
+}
+
+void database::Database_MySQL::disconnect(){
+        
+}
+
+bool database::Database_MySQL::query(const std::string& query,callback_t callback,callback_row_t callback_row){
+    MYSQL *_con;
     _con = mysql_init(nullptr);
     if(_con == nullptr) throw std::logic_error("unable to create connection object for mysql");
     if (mysql_real_connect(_con, "localhost", "root", "3.14Oneer",
           "hl", 0, NULL, 0) == NULL) {
-      std::cout << "connecting to mysql ... fail" << std::endl;
-      mysql_close(_con);
-      throw std::logic_error(mysql_error(_con));
-  }
-}
-
-bool database::Database_MySQL::query(const std::string& query,callback_t callback,callback_row_t callback_row){
-    if (mysql_query(_con, query.c_str()))  throw std::logic_error(query);
-    MYSQL_RES *result = mysql_store_result(_con);
-    if (result == NULL)  throw std::logic_error(query);
-    int num_fields = mysql_num_fields(result);
-    MYSQL_ROW row;
-    int row_num{};
-    while ((row = mysql_fetch_row(result))) { 
-        for(int i = 0; i < num_fields; i++) { 
-            callback(row_num,i,std::string(row[i]));
-        } 
-        callback_row(row_num);
-        ++row_num;
+            mysql_close(_con);
+            throw std::logic_error(mysql_error(_con));
     }
-    mysql_free_result(result);
+
+    try{
+        if (mysql_query(_con, query.c_str()))  throw std::logic_error(query);
+        MYSQL_RES *result = mysql_store_result(_con);
+        if (result == NULL)  throw std::logic_error(query);
+        int num_fields = mysql_num_fields(result);
+        MYSQL_ROW row;
+        int row_num{};
+        while ((row = mysql_fetch_row(result))) { 
+            for(int i = 0; i < num_fields; i++) { 
+                callback(row_num,i,std::string(row[i]));
+            } 
+            callback_row(row_num);
+            ++row_num;
+        }
+        mysql_free_result(result);
+    }catch(...){
+        mysql_close(_con);
+        throw;
+    }
+
+    mysql_close(_con);
     return true;
 }
 
 void database::Database_MySQL::execute(std::string& query){
-    if (mysql_query(_con, query.c_str()))  throw std::logic_error(query);
+    MYSQL *_con;
+    _con = mysql_init(nullptr);
+    if(_con == nullptr) throw std::logic_error("unable to create connection object for mysql");
+    if (mysql_real_connect(_con, "localhost", "root", "3.14Oneer",
+          "hl", 0, NULL, 0) == NULL) {
+            mysql_close(_con);
+            throw std::logic_error(mysql_error(_con));
+    }
+    
+    try{
+            if (mysql_query(_con, query.c_str()))  throw std::logic_error(query);
+     }catch(...){
+        mysql_close(_con);
+        throw;
+    }
+
+    mysql_close(_con);
 }
 
 database::Database_MySQL::~Database_MySQL(){
-    mysql_close(_con);
+    disconnect();
 }
 
 database::Database_MySQL& database::Database_MySQL::get(){
