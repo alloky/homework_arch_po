90d3e3f0d71892f5ba37cb6c753453605e534588
alloky <alloky2011@gmail.com>
2021-11-01 16:12:55 +0300

    fixes

--- code/create_db.sh
+++ code/create_db.sh
@@ -4,4 +4,6 @@ DB="hl";
 USER="root";
 PASS="3.14Oneer";
 
-mysql -u root -p 3.14Oneer -h db -e "CREATE DATABASE IF NOT EXISTS $DB;";
+mysql -uroot -p3.14Oneer -h db -e "CREATE DATABASE IF NOT EXISTS $DB;";
+
+cd build/bin && ./server -db
--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
-curl http://localhost:1234/person?login=adminadmin
-curl http://localhost:1234/person?first_name=testnew&last_name=testnewlast
-curl -d "login=testnew&first_name=testnewname&last_name=testnewlast&age=18"  -X POST "http://localhost:1234/person"
+curl -d "login=testnew&first_name=testnewname&last_name=testnewlast&age=18"  -X POST "http://localhost:1234/person";
+echo "\n===============\n";
+curl -X GET "http://localhost:1234/person?login=testnew";
+echo "\n===============\n";
+curl -X GET "http://localhost:1234/person?first_name=testnewname&last_name=testnewlast";
+echo "\n===============\n";
